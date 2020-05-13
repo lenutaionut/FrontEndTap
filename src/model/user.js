a21978d7148ca5491818ecce0303ee97e38534e8
ionutflorina <florinaionut7@gmail.com>
2020-05-13 13:25:03 +0300

    "some changes"

--- src/model/user.js
+++
@@ -1,8 +0,0 @@
-export default class User {
-    constructor(name, surname, email, password) {
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.password = password;
-    }
-}
